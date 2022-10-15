0a923fe1cd51b7dcdb083f01c413056d99b0f0c2
Olgavav <petrova-olga@myrambler.ru>
2022-10-15 18:19:02 +0400

    Задача 60

---
+++ Task4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+Console.Clear();
+
+ 
+int[,,] array3D = new int[2, 2, 2];
+CreateArray(array3D);
+WriteArray(array3D);
+
+
+void WriteArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write($"{array3D[i, j, k]} ({i}, {j}, {k}) ");
+      }
+    Console.WriteLine();  
+    }
+  }
+}
+
+void CreateArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        array3D[i, j, k] = temp[count];
+        count++;
+      }
+    }
+  }
+}
