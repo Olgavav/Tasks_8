ec2f4ed3a7e3e99cc6d040e770d35d4704c94774
Olgavav <petrova-olga@myrambler.ru>
2022-10-15 12:41:25 +0400

    Задача 56

---
+++ Task2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+Console.Clear();
+
+ 
+int[,] matrix = new int[4, 2];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine(minString(matrix));
+
+
+
+ 
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+        Console.WriteLine();
+    }
+}
+
+ 
+void FillMatrix(int[,] matrix)
+{
+    Random generator = new Random();
+ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        
+            {
+                matrix[i, j] = generator.Next(0,10);
+            }
+    }
+}
+
+int minString (int[,] matrix)
+{
+    int sum = int.MaxValue;
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   int temp = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            temp +=matrix[i, j];
+        }
+        if (temp < sum)
+        {
+            sum = temp;
+            index = i + 1;
+        }
+    }
+return(index);    
+}
+
+
+
